db66f05f2aa79fedd9c1e9907176b5a41ed694a4
Logan Grant <logan.grant1@ocsbstudent.ca>
2021-03-16 16:38:25 +0000

    5-02 Extra

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 document.getElementById('message').addEventListener('click', displayMessage)
 
 function displayMessage () {
-  alert('Hello')
+	alert('Hello')
 }
+
+document.getElementById('color').addEventListener('click', color)
+
+function color () {
+	document.getElementById('paragraph').style.color='red'
+}
+
+document.getElementById('font').addEventListener('click', font)
+
+function font () {
+	document.getElementById('paragraph').style.fontFamily='arial'
+}
+
+document.getElementById('size').addEventListener('click', size)
+
+function size () {
+	document.getElementById('paragraph').style.fontSize='300%'
+}
+
+document.getElementById('reset').addEventListener('click', reset)
